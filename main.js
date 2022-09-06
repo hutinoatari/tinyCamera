3c6d3d68b778771a98e68c0f9a300786da56e891
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-09-07 01:15:35 +0900

    彩度輝度調整機能を破壊

--- main.js
+++ main.js
@@ -4,14 +4,7 @@ const config = {
     width: 240,
     height: 320,
     fps: 2,
-    saturation: 0.5,
-    lightness: 0.5,
-    isEffect: false,
 }
-const saturationRange = document.getElementById("saturationRange");
-saturationRange.addEventListener("change", e => config.saturation = +e.target.value / 100);
-const lightnessRange = document.getElementById("lightnessRange");
-lightnessRange.addEventListener("change", e => config.lightness = +e.target.value / 100);
 
 const video = document.createElement("video");
 video.autoplay = true;
@@ -35,9 +28,6 @@ const context = canvas.getContext("2d");
 const fullscreenButton = document.getElementById("fullscreenButton");
 fullscreenButton.addEventListener("click", () => canvas.requestFullscreen());
 
-const effectChangeButton = document.getElementById("effectChangeButton");
-effectChangeButton.addEventListener("click", () => config.isEffect = !config.isEffect);
-
 const previewScreenUpdate = () => {
     if(video.readyState < HTMLMediaElement.HAVE_METADATA) return;
     const w = video.videoWidth;
@@ -55,87 +45,6 @@ const previewScreenUpdate = () => {
         sy = (h - sh) / 2;
     }
     context.drawImage(video, sx, sy, sw, sh, 0, 0, config.width, config.height);
-    if(!config.isEffect) return;
-
-    const imageData = context.getImageData(0, 0, config.width, config.height);
-    const newImageData = context.createImageData(config.width, config.height);
-    const len = imageData.width * imageData.height * 4;
-    for(let i=0; i<len; i+=4){
-        let r, g, b, h, l, s, max, min, diff, t;
-        [r, g, b] = imageData.data.slice(i, i+3);
-        r /= 255;
-        g /= 255;
-        b /= 255;
-        max = Math.max(r, g, b);
-        min = Math.min(r, g, b);
-        diff = max - min;
-        h = 0;
-        l = (max + min) / 2;
-        s = diff / (1 -Math.abs(max + min - 1));
-        switch(min){
-            case max: h = 0; break;
-            case r: h = (60 * ((b - g) / diff)) + 180; break;
-            case g: h = (60 * ((r - b) / diff)) + 300; break;
-            case b: h = (60 * ((g - r) / diff)) + 60; break;
-        }
-
-        if(config.saturation > 0.5){
-            s = s + (1-s)*(config.saturation-0.5)*2;
-        }else{
-            s = s * (config.saturation) * 2;
-        }
-        if(config.lightness > 0.5){
-            l = l + (1-l)*(config.lightness-0.5)*2;
-        }else{
-            l = l * (config.lightness) * 2;
-        }
-
-        max = l + (s * (1 - Math.abs((2 * l) - 1)) / 2);
-        min = l - (s * (1 - Math.abs((2 * l) - 1)) / 2);
-        t = parseInt(h/60);
-        switch(t){
-            case 0:
-            case 6:
-                r = max;
-                g = min + (max - min) * h / 60;
-                b = min;
-                break;
-            case 1:
-                r = min + (max - min) * (120 - h) / 60;
-                g = max;
-                b = min;
-                break;
-            case 2:
-                r = min;
-                g = max;
-                b = min + (max - min) * (h - 120) / 60;
-                break;
-            case 3:
-                r = min;
-                g = min + (max - min) * (240 - h) / 60;
-                b = max;
-                break;
-            case 4:
-                r = min + (max - min) * (h - 240) / 60;
-                g = min;
-                b = max;
-                break;
-            case 5:
-                r = max;
-                g = min;
-                b = min + (max - min) * (360 - h) / 60;
-                break;
-        }
-        r *= 255;
-        g *= 255;
-        b *= 255;
-
-        newImageData.data[i] = r;
-        newImageData.data[i+1] = g;
-        newImageData.data[i+2] = b;
-        newImageData.data[i+3] = 255;
-    }
-    context.putImageData(newImageData, 0, 0);
 }
 setInterval(previewScreenUpdate, 1000/config.fps);
 
